ab158d912dca398e1c72c3a973f1942373f01ef0
VladiusVostokus <kovalenkov222@gmail.com>
2024-11-30 16:54:08 +0200

    Put lid in correct position

--- task.js
+++ task.js
@@ -110,48 +110,49 @@ function main() {
     ]);
 
     const lidBufferData = new Float32Array([
-        -.5, 0.5, -0.5,   0,1,1,
-        -.5, 0.6,  0.5,   0,1,1,
-        -.5, 0.6, -0.5,   0,1,1,
-        -.5, 0.5,  0.5,   0,1,1,
-        -.5, 0.6,  0.5,   0,1,1,
-        -.5, 0.5, -0.5,   0,1,1,
-    
-         0.5, 0.5, -0.5,   1,0,1,
-         0.5, 0.6, -0.5,   1,0,1,
-         0.5, 0.6,  0.5,   1,0,1,
-         0.5, 0.6,  0.5,   1,0,1,
-         0.5, 0.5,  0.5,   1,0,1,
-         0.5, 0.5, -0.5,   1,0,1,
-    
-        -.5, 0.5, -0.5,   0,1,0,
-         0.5, 0.5, -0.5,   0,1,0,
-         0.5, 0.5,  0.5,   0,1,0,
-         0.5, 0.5,  0.5,   0,1,0,
-        -.5, 0.5,  0.5,   0,1,0,
-        -.5, 0.5, -0.5,   0,1,0,
-    
-        -.5, 0.6, -0.5,   1,1,0,
-         0.5, 0.6,  0.5,   1,1,0,
-         0.5, 0.6, -0.5,   1,1,0,
-        -.5, 0.6,  0.5,   1,1,0,
-         0.5, 0.6,  0.5,   1,1,0,
-        -.5, 0.6, -0.5,   1,1,0,
-    
-         0.5, 0.5, -0.5,   0,0,1,
-        -.5, 0.5, -0.5,   0,0,1,
-         0.5, 0.6, -0.5,   0,0,1,
-        -.5, 0.6, -0.5,   0,0,1,
-         0.5, 0.6, -0.5,   0,0,1,
-        -.5, 0.5, -0.5,   0,0,1,
-    
-        -.5, 0.5,  0.5,   1,0,0,
-         0.5, 0.5,  0.5,   1,0,0,
-         0.5, 0.6,  0.5,   1,0,0,
-         0.5, 0.6,  0.5,   1,0,0,
-        -.5, 0.6,  0.5,   1,0,0,
-        -.5, 0.5,  0.5,   1,0,0,
-    ]);
+      -0.5,  0,    0,       0,1,1,
+      -0.5,  0.1,  1,       0,1,1,
+      -0.5,  0.1,  0,       0,1,1,
+      -0.5,  0,    1,       0,1,1,
+      -0.5,  0.1,  1,       0,1,1,
+      -0.5,  0,    0,       0,1,1,
+  
+       0.5,  0,    0,       1,0,1,
+       0.5,  0.1,  0,       1,0,1,
+       0.5,  0.1,  1,       1,0,1,
+       0.5,  0.1,  1,       1,0,1,
+       0.5,  0,    1,       1,0,1,
+       0.5,  0,    0,       1,0,1,
+  
+      -0.5,  0,    0,       0,1,0,
+       0.5,  0,    0,       0,1,0,
+       0.5,  0,    1,       0,1,0,
+       0.5,  0,    1,       0,1,0,
+      -0.5,  0,    1,       0,1,0,
+      -0.5,  0,    0,       0,1,0,
+  
+      -0.5,  0.1,  0,       1,1,0,
+       0.5,  0.1,  1,       1,1,0,
+       0.5,  0.1,  0,       1,1,0,
+      -0.5,  0.1,  1,       1,1,0,
+       0.5,  0.1,  1,       1,1,0,
+      -0.5,  0.1,  0,       1,1,0,
+  
+       0.5,  0,    0,       0,0,1,
+      -0.5,  0,    0,       0,0,1,
+       0.5,  0.1,  0,       0,0,1,
+      -0.5,  0.1,  0,       0,0,1,
+       0.5,  0.1,  0,       0,0,1,
+      -0.5,  0,    0,       0,0,1,
+  
+      -0.5,  0,    1,       1,0,0,
+       0.5,  0,    1,       1,0,0,
+       0.5,  0.1,  1,       1,0,0,
+       0.5,  0.1,  1,       1,0,0,
+      -0.5,  0.1,  1,       1,0,0,
+      -0.5,  0,    1,       1,0,0,
+  ]);
+  
 
 
     const buffer = gl.createBuffer();
@@ -228,7 +229,7 @@ function main() {
             1,0,0,0,
             0,cos2,sin2,0,
             0,-sin2,cos2,0,
-            0, deltaX,deltaX,1,
+            0,0.65,-0.2,1,
         ]);
         
         gl.uniformMatrix4fv(uProjectionMatrix_Y, false, projectionMatrix_Y);
@@ -236,12 +237,13 @@ function main() {
         gl.uniformMatrix4fv(uPerspectiveMatrix, false, perspectiveMatrix);
         gl.uniformMatrix4fv(uModelViewMatrix, false, modelViewMatrix);
 
+        
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
         gl.bufferData(gl.ARRAY_BUFFER, crateBufferData, gl.STATIC_DRAW);
 
         gl.vertexAttribPointer(aPosition, 3 , gl.FLOAT, false, 6 * 4, 0);
         gl.vertexAttribPointer(aColor, 3 , gl.FLOAT, false, 6 * 4, 3 * 4);
-        gl.drawArrays(gl.TRIANGLES, 0, 36);
+        gl.drawArrays(gl.TRIANGLES, 0, 36); 
 
         gl.uniformMatrix4fv(uProjectionMatrix_X, false, projectionMatrix_X2);
 
@@ -269,7 +271,7 @@ function main() {
             }
         }
         if (e.key === "ArrowDown") {
-            if (deltaAngle != 60) {
+            if (deltaAngle != 0) {
                 deltaAngle += 30;
                 deltaX -= 0.3;
             }
@@ -278,6 +280,95 @@ function main() {
     });
 }
 
+var m4 = {
+    translation: function(tx, ty, tz) {
+      return [
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        tx, ty, tz, 1,
+      ];
+    },
+  
+    rotationX: function(angleInRadians) {
+      var c = Math.cos(angleInRadians);
+      var s = Math.sin(angleInRadians);
+      return [
+        1, 0,  0, 0,
+        0, c, -s, 0,
+        0, s,  c, 0,
+        0, 0,  0, 1,
+      ];
+    },
+  
+    rotationY: function(angleInRadians) {
+      var c = Math.cos(angleInRadians);
+      var s = Math.sin(angleInRadians);
+      return [
+         c, 0, s, 0,
+         0, 1, 0, 0,
+        -s, 0, c, 0,
+         0, 0, 0, 1,
+      ];
+    },
+  
+    rotationZ: function(angleInRadians) {
+      var c = Math.cos(angleInRadians);
+      var s = Math.sin(angleInRadians);
+      return [
+        c, -s, 0, 0,
+        s,  c, 0, 0,
+        0,  0, 1, 0,
+        0,  0, 0, 1,
+      ];
+    },
+  
+    multiply: function(a, b) {
+      var a00 = a[0 * 4 + 0], a01 = a[0 * 4 + 1], a02 = a[0 * 4 + 2], a03 = a[0 * 4 + 3];
+      var a10 = a[1 * 4 + 0], a11 = a[1 * 4 + 1], a12 = a[1 * 4 + 2], a13 = a[1 * 4 + 3];
+      var a20 = a[2 * 4 + 0], a21 = a[2 * 4 + 1], a22 = a[2 * 4 + 2], a23 = a[2 * 4 + 3];
+      var a30 = a[3 * 4 + 0], a31 = a[3 * 4 + 1], a32 = a[3 * 4 + 2], a33 = a[3 * 4 + 3];
+      var b00 = b[0 * 4 + 0], b01 = b[0 * 4 + 1], b02 = b[0 * 4 + 2], b03 = b[0 * 4 + 3];
+      var b10 = b[1 * 4 + 0], b11 = b[1 * 4 + 1], b12 = b[1 * 4 + 2], b13 = b[1 * 4 + 3];
+      var b20 = b[2 * 4 + 0], b21 = b[2 * 4 + 1], b22 = b[2 * 4 + 2], b23 = b[2 * 4 + 3];
+      var b30 = b[3 * 4 + 0], b31 = b[3 * 4 + 1], b32 = b[3 * 4 + 2], b33 = b[3 * 4 + 3];
+  
+      return [
+        b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+        b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+        b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+        b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+        b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+        b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+        b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+        b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+        b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+        b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+        b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+        b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+        b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+        b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+        b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+        b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+      ];
+    },
+  
+    translate: function(m, tx, ty, tz) {
+      return m4.multiply(m, m4.translation(tx, ty, tz));
+    },
+  
+    rotateX: function(m, angleInRadians) {
+      return m4.multiply(m, m4.rotationX(angleInRadians));
+    },
+  
+    rotateY: function(m, angleInRadians) {
+      return m4.multiply(m, m4.rotationY(angleInRadians));
+    },
+  
+    rotateZ: function(m, angleInRadians) {
+      return m4.multiply(m, m4.rotationZ(angleInRadians));
+    },
+  };
 /* TODO: потрібно зробити:
 1) Окремі функції малювання для кришки і коробки(тут можна просто передавати різні координати)
 2) Для кришки та коробки мають бути різні шейдерні програми, тобто коди шейдрів для кришки і коробки дублюватимуься,
