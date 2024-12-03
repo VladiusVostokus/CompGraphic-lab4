e9ceb6f56ad70fb69620c9e0047d7d66cc3901c9
VladiusVostokus <kovalenkov222@gmail.com>
2024-12-03 12:02:14 +0200

    Multiply X and Y matrixes in JS code instead of vertex code

--- task.js
+++ task.js
@@ -4,13 +4,12 @@ const vsSource = `#version 300 es
 in vec3 aColor;
 out vec3 vColor;
 in vec3 aPosition;
-uniform mat4 uProjectionMatrix_Y;
-uniform mat4 uProjectionMatrix_X;
+uniform mat4 uTransormMatrix;
 uniform mat4 uPerspectiveMatrix;
 uniform mat4 uModelViewMatrix;
 
 void main() {
-    gl_Position = uPerspectiveMatrix * (uModelViewMatrix * uProjectionMatrix_Y * uProjectionMatrix_X * vec4(aPosition, 1.0));
+    gl_Position = uPerspectiveMatrix * (uModelViewMatrix * uTransormMatrix * vec4(aPosition, 1.0));
     vColor = aColor;
 }`;
 
@@ -60,8 +59,7 @@ function main() {
 
     const aColor = gl.getAttribLocation(program,'aColor');
     const aPosition = gl.getAttribLocation(program, 'aPosition');
-    const uProjectionMatrix_Y = gl.getUniformLocation(program,'uProjectionMatrix_Y');
-    const uProjectionMatrix_X = gl.getUniformLocation(program,'uProjectionMatrix_X');
+    const uTransormMatrix = gl.getUniformLocation(program,'uTransormMatrix');
     const uPerspectiveMatrix = gl.getUniformLocation(program,"uPerspectiveMatrix");
     const uModelViewMatrix = gl.getUniformLocation(program, "uModelViewMatrix");
 
@@ -189,9 +187,38 @@ function main() {
         return matrix;
     }
 
+    const multiply = (a, b) => {
+        var a00 = a[0 * 4 + 0], a01 = a[0 * 4 + 1], a02 = a[0 * 4 + 2], a03 = a[0 * 4 + 3];
+        var a10 = a[1 * 4 + 0], a11 = a[1 * 4 + 1], a12 = a[1 * 4 + 2], a13 = a[1 * 4 + 3];
+        var a20 = a[2 * 4 + 0], a21 = a[2 * 4 + 1], a22 = a[2 * 4 + 2], a23 = a[2 * 4 + 3];
+        var a30 = a[3 * 4 + 0], a31 = a[3 * 4 + 1], a32 = a[3 * 4 + 2], a33 = a[3 * 4 + 3];
+        var b00 = b[0 * 4 + 0], b01 = b[0 * 4 + 1], b02 = b[0 * 4 + 2], b03 = b[0 * 4 + 3];
+        var b10 = b[1 * 4 + 0], b11 = b[1 * 4 + 1], b12 = b[1 * 4 + 2], b13 = b[1 * 4 + 3];
+        var b20 = b[2 * 4 + 0], b21 = b[2 * 4 + 1], b22 = b[2 * 4 + 2], b23 = b[2 * 4 + 3];
+        var b30 = b[3 * 4 + 0], b31 = b[3 * 4 + 1], b32 = b[3 * 4 + 2], b33 = b[3 * 4 + 3];
+      
+        return [
+          b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+          b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+          b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+          b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+          b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+          b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+          b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+          b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+          b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+          b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+          b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+          b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+          b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+          b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+          b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+          b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+        ];
+      }
+
     let angle = 30;
     let deltaAngle = 0;
-    let deltaX = 0;
     let fixedAngle = 30;
     const fixedRadian = Math.PI * fixedAngle / 180;
     const fixedCos = Math.cos(fixedRadian);
@@ -221,6 +248,8 @@ function main() {
             0,0,0,1,
         ]);
 
+        const transformMatrix1 = multiply(projectionMatrix_Y, projectionMatrix_X);
+
         const radian2 = Math.PI * (fixedAngle + deltaAngle) / 180;
         const cos2 = Math.cos(radian2);
         const sin2 = Math.sin(radian2);
@@ -231,9 +260,10 @@ function main() {
             0,-sin2,cos2,0,
             0,0.65,-0.2,1,
         ]);
+
+        const transformMatrix2 = multiply(projectionMatrix_Y, projectionMatrix_X2);
         
-        gl.uniformMatrix4fv(uProjectionMatrix_Y, false, projectionMatrix_Y);
-        gl.uniformMatrix4fv(uProjectionMatrix_X, false, projectionMatrix_X);
+        gl.uniformMatrix4fv(uTransormMatrix, false, transformMatrix1);
         gl.uniformMatrix4fv(uPerspectiveMatrix, false, perspectiveMatrix);
         gl.uniformMatrix4fv(uModelViewMatrix, false, modelViewMatrix);
 
@@ -245,7 +275,7 @@ function main() {
         gl.vertexAttribPointer(aColor, 3 , gl.FLOAT, false, 6 * 4, 3 * 4);
         gl.drawArrays(gl.TRIANGLES, 0, 36); 
 
-        gl.uniformMatrix4fv(uProjectionMatrix_X, false, projectionMatrix_X2);
+        gl.uniformMatrix4fv(uTransormMatrix, false, transformMatrix2);
 
         gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
         gl.bufferData(gl.ARRAY_BUFFER, lidBufferData, gl.STATIC_DRAW);
@@ -260,119 +290,23 @@ function main() {
     window.addEventListener("keydown", (e) => {
         if (e.key === "ArrowLeft") {
             angle -= 10;
+            draw();
         }
         if (e.key === "ArrowRight") {
             angle += 10;
+            draw();
         }
         if (e.key === "ArrowUp") {
-            if (deltaAngle != -60) {
-                deltaAngle -= 30;
-                deltaX += 0.3;
+            if (deltaAngle > -60) {
+                deltaAngle -= 10;
             }
+            draw();
         }
         if (e.key === "ArrowDown") {
-            if (deltaAngle != 0) {
-                deltaAngle += 30;
-                deltaX -= 0.3;
+            if (deltaAngle < 0) {
+                deltaAngle += 10;
             }
+            draw();
         }
-        draw();
     });
 }
-
-var m4 = {
-    translation: function(tx, ty, tz) {
-      return [
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        tx, ty, tz, 1,
-      ];
-    },
-  
-    rotationX: function(angleInRadians) {
-      var c = Math.cos(angleInRadians);
-      var s = Math.sin(angleInRadians);
-      return [
-        1, 0,  0, 0,
-        0, c, -s, 0,
-        0, s,  c, 0,
-        0, 0,  0, 1,
-      ];
-    },
-  
-    rotationY: function(angleInRadians) {
-      var c = Math.cos(angleInRadians);
-      var s = Math.sin(angleInRadians);
-      return [
-         c, 0, s, 0,
-         0, 1, 0, 0,
-        -s, 0, c, 0,
-         0, 0, 0, 1,
-      ];
-    },
-  
-    rotationZ: function(angleInRadians) {
-      var c = Math.cos(angleInRadians);
-      var s = Math.sin(angleInRadians);
-      return [
-        c, -s, 0, 0,
-        s,  c, 0, 0,
-        0,  0, 1, 0,
-        0,  0, 0, 1,
-      ];
-    },
-  
-    multiply: function(a, b) {
-      var a00 = a[0 * 4 + 0], a01 = a[0 * 4 + 1], a02 = a[0 * 4 + 2], a03 = a[0 * 4 + 3];
-      var a10 = a[1 * 4 + 0], a11 = a[1 * 4 + 1], a12 = a[1 * 4 + 2], a13 = a[1 * 4 + 3];
-      var a20 = a[2 * 4 + 0], a21 = a[2 * 4 + 1], a22 = a[2 * 4 + 2], a23 = a[2 * 4 + 3];
-      var a30 = a[3 * 4 + 0], a31 = a[3 * 4 + 1], a32 = a[3 * 4 + 2], a33 = a[3 * 4 + 3];
-      var b00 = b[0 * 4 + 0], b01 = b[0 * 4 + 1], b02 = b[0 * 4 + 2], b03 = b[0 * 4 + 3];
-      var b10 = b[1 * 4 + 0], b11 = b[1 * 4 + 1], b12 = b[1 * 4 + 2], b13 = b[1 * 4 + 3];
-      var b20 = b[2 * 4 + 0], b21 = b[2 * 4 + 1], b22 = b[2 * 4 + 2], b23 = b[2 * 4 + 3];
-      var b30 = b[3 * 4 + 0], b31 = b[3 * 4 + 1], b32 = b[3 * 4 + 2], b33 = b[3 * 4 + 3];
-  
-      return [
-        b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
-        b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
-        b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
-        b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
-        b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
-        b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
-        b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
-        b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
-        b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
-        b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
-        b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
-        b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
-        b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
-        b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
-        b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
-        b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
-      ];
-    },
-  
-    translate: function(m, tx, ty, tz) {
-      return m4.multiply(m, m4.translation(tx, ty, tz));
-    },
-  
-    rotateX: function(m, angleInRadians) {
-      return m4.multiply(m, m4.rotationX(angleInRadians));
-    },
-  
-    rotateY: function(m, angleInRadians) {
-      return m4.multiply(m, m4.rotationY(angleInRadians));
-    },
-  
-    rotateZ: function(m, angleInRadians) {
-      return m4.multiply(m, m4.rotationZ(angleInRadians));
-    },
-  };
-/* TODO: потрібно зробити:
-1) Окремі функції малювання для кришки і коробки(тут можна просто передавати різні координати)
-2) Для кришки та коробки мають бути різні шейдерні програми, тобто коди шейдрів для кришки і коробки дублюватимуься,
-але можна зробити 1 ф. для створення програми, в яку просто передавати різні шейдри і вона повератиме різні програми,
-3) По черзі викликати ф. малювання кришки і коробки, передаючи їм матриці перетворення і біндити буфер на різні вершини
-(коробки і кришки), можливо, по черзі викликаючи програми. 
-*/
